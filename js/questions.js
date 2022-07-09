83b183a2ebcd2f270fe95ef5a97653cc051c2bbf
enterusernamee <103475505+enterusernamee@users.noreply.github.com>
2022-07-09 13:37:05 -0500

    added questions.js folder and added test questions

---
+++ js/questions.js
@@ -0,0 +1,19 @@
+var questions = [
+    {
+        title: "test question 1",
+        choices: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 2"
+    },
+
+    {
+        title: "test question 2",
+        choices: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 1"
+    },
+
+    {
+        title: "test question 3",
+        choices: ["answer 1", "answer 2", "answer 3", "answer 4"],
+        answer: "answer 4"
+    }
+];
---
+++ js/script.js
@@ -0,0 +1,22 @@
+var startBox = document.getElementById("startBox");
+var questionBox = document.getElementById("questionBox");
+var startButton = document.getElementById("startButton");
+var choices = document.getElementById("choices");
+
+function startQuiz() {
+    console.log("start button clicked")
+    startBox.style.display = "none";
+    questionBox.style.display = "block"
+    getQuestions()
+}
+
+function getQuestions() {
+    var currentQuestion= 0
+    var currentChoices = 0
+    questionBox.textContent = questions[currentQuestion].title
+    choices.textContent = questions[currentChoices].choices
+    console.log(choices)
+}
+
+startButton.addEventListener("click", startQuiz)
+
