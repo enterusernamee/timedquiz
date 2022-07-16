81292d29462090a48baa3ab65c59097c40f7dd14
enterusernamee <103475505+enterusernamee@users.noreply.github.com>
2022-07-16 17:47:21 -0500

    deleted some

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+var questions = [
+    {
+        title: "Arrays in JS can be used:",
+        choices: ["numbers and strings", "other arrays", "boolean", "all of the above"],
+        answer: "all of the above"
+    },
+
+    {
+        title: "Commonly used data types do NOT include:",
+        choices: ["strings", "boolean", "alert", "number"],
+        answer: "boolean"
+    },
+
+    {
+        title: "String values must be closed within ____ when being assigned to variables",
+        choices: ["comma", "curly bracket", "quotes", "parenthesis"],
+        answer: "curly bracket"
+    },
+
+    {
+        title: "Condition in an if/else statement is closed with:",
+        choices: ["quotes", "curly brackets", "parenthesis", "square bracket"],
+        answer: "parenthesis"
+    },
+
+    {
+        title: "A useful tool during development and debugging for printing content:",
+        choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
+        answer: "for loops"
+    }
+]
+
 var startBox = document.getElementById("startBox");
 var questionBox = document.getElementById("questionBox");
 var startButton = document.getElementById("startButton");
@@ -10,6 +42,9 @@ function startQuiz() {
     getQuestions()
 }
 
+
+
+
 function getQuestions() {
     var currentQuestion= 0
     var currentChoices = 0
@@ -17,11 +52,11 @@ function getQuestions() {
     choices.textContent = questions[currentChoices].choices
     
     console.log(choices)
-    // currentQuestion.choices.forEach(function(choices, i){
-    //     var choicesList= document.createElement("button")
-    //     choicesList.setAttribute("class", "choice")
-    //     choicesList.onClick= questionClick
-    // })
+     currentQuestion.choices.forEach(function(choices, i){
+         var choicesList= document.createElement("button")
+         choicesList.setAttribute("class", "choice")
+         choicesList.onClick= questionClick
+     })
 }
 
 
